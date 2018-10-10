ebf7237320f8676f38315d14ea3188d28d0ac249
micheal <michealdunne14@gmail.com>
2018-10-10 11:43:00 +0100

    First commit

--- models/foodList.js
+++ models/foodList.js
@@ -8,11 +8,3 @@ let FoodSchema = new mongoose.Schema({
     { collection: 'foodsdb' });
 
 module.exports = mongoose.model('Food', FoodSchema);
-
-/*const foodList = [
-    {id: 1000000, coursedinner: 'Starter', fooditem: 'ceasar salad', upvotes: 1},
-    {id: 1000001, coursedinner: 'Main', fooditem: 'chicken stir fry', upvotes: 2},
-    {id: 1000002, coursedinner: 'Desert', fooditem: 'death by chocolate', upvotes: 100}
-];
-
-module.exports = foodList;*/
