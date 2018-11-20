86e7ad1c2ef3450f67fbee5c3943ec7f76b70f50
micheal <michealdunne14@gmail.com>
2018-11-20 12:52:11 +0000

    Edits to Readme

--- models/foodList.js
+++ models/foodList.js
@@ -5,7 +5,7 @@ let FoodSchema = new mongoose.Schema({
         coursedinner: {type: String, default: ""},
         fooditem: {type: String, default: ""},
         upvotes: {type: Number, default: 0},
-        downvotes: {type: Number, default: 0}
+        downvotes: {type: Number, default: 0},
     },
     {collection: 'foodsdb'});
 
